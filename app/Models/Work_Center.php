420ddb89c20b0a1d30d1b544b48c9b4d39175d97
becallgit <daniel.mardomingo@becallgroup.com>
2025-02-26 13:44:17 +0100

    creacion del modelo faltante work_center

---
+++ app/Models/Work_Center.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Work_Center extends Model
+{
+    protected $fillable = [
+        'name'
+       
+    ];
+   protected $table = "work_center";
+
+}
